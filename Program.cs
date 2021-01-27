2e3629b5ba17b0b17e5374bf35c1b7329b766827
sheemat <48071652+sheemat@users.noreply.github.com>
2021-01-26 22:26:43 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//TEST223111
+//TEST2231117
 namespace blazorSolution
 {
     public class Program
