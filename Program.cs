bac2432b7e6f0d7c2e7c0df1a4c2625b26369527
sheemat <48071652+sheemat@users.noreply.github.com>
2021-01-26 22:24:57 -0600

    Comented6

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//TEST22311
+//TEST223111
 namespace blazorSolution
 {
     public class Program
